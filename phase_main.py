7d1620a88c9e4c8932ee425a9b06c758a49b8586
Daniel Malmer <dmalmer@gmail.com>
2016-05-27 11:58:22 -0600

    collect stats from Locus objects

--- phase_main.py
+++ phase_main.py
@@ -2,49 +2,8 @@
 import sys
 from collections import defaultdict
 
-from util import chrom_sort, read_ped, read_vcf, output_phased_vcf
+from util import read_ped, read_vcf, output_phased_vcf
 from mendel import find_nondisjunct_parent, phase_trio_diploid, phase_trio_trisomy
-
-
-def phase(diploid_alleles, trisomy_alleles):
-    phases_by_chr = defaultdict(list)
-    unique_phases = {}
-
-    # diploid phasing
-    for child_alleles, dad_alleles, mom_alleles, sibling_alleles, chrom, pos, child_col_str, dad_col_str, mom_col_str, \
-            sibling_col_str, line_pre_str in diploid_alleles: 
-        # inheritance phasing
-        #  phase proband child
-        child_phased_GT, dad_phased_GT, mom_phased_GT, is_mendelian = phase_trio_diploid(child_alleles, dad_alleles, mom_alleles)
-        #  phase sibling (if available)
-        sibling_phased_GT = ''
-        if len(sibling_alleles) > 0:
-            sibling_phased_GT, _, _, _ = phase_trio_diploid(sibling_alleles, dad_alleles, mom_alleles)
-    
-        # add phase (pos, child_phase, dad_phase, mom_phase, is_mendelian, line_pre_str)
-        phases_by_chr[chrom].append((pos, child_phased_GT + child_col_str, dad_phased_GT + dad_col_str, 
-                                     mom_phased_GT + mom_col_str, sibling_phased_GT + sibling_col_str, is_mendelian, line_pre_str))
-
-        k = '_'.join((child_phased_GT, dad_phased_GT, mom_phased_GT))
-        if k not in unique_phases:
-            unique_phases[k] = (child_phased_GT, dad_phased_GT, mom_phased_GT, child_alleles, dad_alleles, mom_alleles, is_mendelian, '')
-    
-    # trisomy phasing
-    if len(trisomy_alleles) > 0:
-        nondisjunct_parent = find_nondisjunct_parent(trisomy_alleles)
-        for child_alleles, dad_alleles, mom_alleles, chrom, pos, child_col_str, dad_col_str, mom_col_str, line_pre_str in trisomy_alleles:
-            # inheritance phasing
-            child_phased_GT, dad_phased_GT, mom_phased_GT, is_mendelian = phase_trio_trisomy(child_alleles, dad_alleles, mom_alleles, nondisjunct_parent)
-                
-            # add phase (pos, child_phase, dad_phase, mom_phase, is_mendelian, line_pre_str)
-            phases_by_chr[chrom].append((pos, child_phased_GT + child_col_str, dad_phased_GT + dad_col_str, 
-                                        mom_phased_GT + mom_col_str, is_mendelian, line_pre_str))
-
-            k = '_'.join(('_'.join(child_alleles), '_'.join(dad_alleles), '_'.join(mom_alleles), nondisjunct_parent))
-            if k not in unique_phases:
-                unique_phases[k] = (child_phased_GT, dad_phased_GT, mom_phased_GT, child_alleles, dad_alleles, mom_alleles, is_mendelian, nondisjunct_parent)
-    
-    return phases_by_chr, unique_phases
 
 
 if __name__ == '__main__':
@@ -66,29 +25,19 @@ if __name__ == '__main__':
     rels = read_ped(ped_file)
 
     # read vcf file and set nondisjunction parent
-    alleles_by_chr, header = read_vcf(filename, rels, fam_size)
-    trisomy_alleles = [locus for allele_list in alleles_by_chr.values() for locus in allele_list if locus.ploidy == 3]
-    nondisjunct_parent = find_nondisjunct_parent(trisomy_alleles)
-    for locus in trisomy_alleles:
+    loci_by_chr, header = read_vcf(filename, rels, fam_size)
+    trisomy_loci = [locus for loci_list in loci_by_chr.values() for locus in loci_list if locus.ploidy == 3]
+    nondisjunct_parent = find_nondisjunct_parent(trisomy_loci)
+    for locus in trisomy_loci:
         locus.nondisjunct_parent = nondisjunct_parent
 
     # phase
-    #TODO: FINISH
-    phases_by_chr, unique_phases = phase(diploid_alleles, trisomy_alleles)
-            
-    # print results
-    #for k in unique_phases:
-    #    print '\nphase:'
-    #    print 'dad_alleles = %i' % unique_phases[k][4]
-    #    print 'mom_alleles = %i' % unique_phases[k][5]
-    #    print 'child_alleles = %i' % unique_phases[k][3]
-    #    print 'dad_GT = %i' % unique_phases[k][1]
-    #    print 'mom_GT = %i' % unique_phases[k][2]
-    #    print 'child_GT = %i' % unique_phases[k][0]
-    #    print 'is_mendelian = %i' % unique_phases[k][6]
-    #    print 'nondisjunct_parent = %i' % unique_phases[k][7]
+    for loci_list in loci_by_chr.values():
+        for locus in loci_list:
+            locus.phase()
 
-    print '\n\nPHASE COUNTS:'
+    # collect statistics and track phased regions
+    phased_regions_by_chr = defaultdict(list)
     dip_het_mend_phased = 0
     dip_het_mend_unphased = 0
     dip_hom_mend_phased = 0
@@ -100,60 +49,59 @@ if __name__ == '__main__':
     tri_hom_mend_phased = 0
     tri_hom_mend_unphased = 0
     tri_nonmend = 0
-    phased_regions = []
-    curr_region = ['', '', '']
-    for curr_chr in chrom_sort(phases_by_chr.keys()):
-        for pos, child_phased_col, dad_phased_col, mom_phased_col, is_mendelian, line_pre in phases_by_chr[curr_chr]:
-            if is_mendelian:
-                child_GT = child_phased_col.split(':')[0]
-                dad_GT = dad_phased_col.split(':')[0]
-                mom_GT = mom_phased_col.split(':')[0]
-                alleles = set()
-                alleles.update(child_GT.replace('|',',').replace('/',',').split(','))
-                alleles.update(dad_GT.replace('|',',').replace('/',',').split(','))
-                alleles.update(mom_GT.replace('|',',').replace('/',',').split(','))
-                if '/' in child_GT + dad_GT + mom_GT:
+    tri_phased_by_brother = 0
+    for chrom, loci_list in loci_by_chr.items():
+        curr_region = ['', '', '']
+        for locus in loci_list: 
+            if locus.is_mendelian:
+                alleles = set(locus.child_alleles + locus.dad_alleles + locus.mom_alleles)
+                if '/' in locus.child_phased_GT + locus.dad_phased_GT + locus.mom_phased_GT:
                     if len(alleles) > 1:
-                        if 'chr21' not in curr_chr:
+                        if locus.ploidy == 2:
                             dip_het_mend_unphased += 1
                         else:
                             tri_het_mend_unphased += 1
-                            if '|' in child_GT:
+                            if '|' in locus.child_phased_GT:
                                 tri_het_mend_disj_phased += 1
                     else:
-                        if 'chr21' not in curr_chr:
+                        if locus.ploidy == 2:
                             dip_hom_mend_unphased += 1
                         else:
                             tri_hom_mend_unphased += 1
                     
                     # reset current phased region
                     if curr_region[1] != '':
-                        phased_regions.append(curr_region)
+                        phased_regions_by_chr[locus.chrom].append(curr_region)
                     curr_region = ['', '', '']
 
                 else:
                     if len(alleles) > 1:
-                        if 'chr21' not in curr_chr:
+                        if locus.ploidy == 2:
                             dip_het_mend_phased += 1
                         else:
                             tri_het_mend_phased += 1
                     else:
-                        if 'chr21' not in curr_chr:
+                        if locus.ploidy == 2:
                             dip_hom_mend_phased += 1
                         else:
                             tri_hom_mend_phased += 1
                     
                     # add to current phased region
-                    curr_region[2] = pos
+                    curr_region[2] = locus.pos
                     if curr_region[1] == '':
-                        curr_region[0] = curr_chr
-                        curr_region[1] = pos
+                        curr_region[0] = locus.chrom
+                        curr_region[1] = locus.pos
 
             else:
-                if 'chr21' not in curr_chr:
+                if locus.ploidy == 2:
                     dip_nonmend += 1
                 else:
                     tri_nonmend += 1
+
+                # reset current phased region
+                if curr_region[1] != '':
+                    phased_regions_by_chr[locus.chrom].append(curr_region)
+                curr_region = ['', '', '']
 
     def print_division(print_str, numerator, denominator):
         print print_str,
@@ -162,6 +110,7 @@ if __name__ == '__main__':
         except ZeroDivisionError:
             print float(numerator), '/ 0 (division by zero exception caught)'
 
+    print '\n\nPHASE COUNTS:'
     print ' DIPLOID:'
     print '  dip_het_mend_phased = %i' % dip_het_mend_phased
     print '  dip_het_mend_unphased = %i' % dip_het_mend_unphased
