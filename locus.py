5b8d5b5a5681d6e972595b83ede44192a6dfebbc
Daniel Malmer <dmalmer@gmail.com>
2016-06-19 11:54:52 -0600

    phase ambiguous trisomy positions by leveraging the quartet sibling

--- locus.py
+++ locus.py
@@ -18,30 +18,50 @@ class Locus:
         self.sibling_col_str = sibling_col_str
 
         self.ploidy = len(child_alleles)
-        self.nondisjunction_parent = None
+        self.nondisjunct_parent = None
 
         self.child_phased_GT = None
         self.dad_phased_GT = None
         self.mom_phased_GT = None
         self.sibling_phased_GT = None
 
+        self.is_mendelian = False
+        self.is_phased = False
+        self.is_heterozygous = len(set(self.child_alleles)) != 1
+        self.is_phased_by_sibling = False
+
 
     def phase(self):
-        #self phased_GT variables
+        #set phased_GT, is_mendelian, and is_phased_by_sibling variables
         if self.ploidy == 2:
-            self.child_phased_GT, self.dad_phased_GT, self.mom_phased_GT = \
+            self.child_phased_GT, self.dad_phased_GT, self.mom_phased_GT, self.is_mendelian = \
                     phase_trio_diploid(self.child_alleles, self.dad_alleles, self.mom_alleles) 
-            if sibling_alleles != None:
-                self.sibling_phased_GT, _, _ = phase_trio_diploid(self.sibling_alleles, self.dad_alleles, self.mom_alleles)
-
+            if self.sibling_alleles is not None:
+                self.sibling_phased_GT, _, _, _ = phase_trio_diploid(self.sibling_alleles, self.dad_alleles, self.mom_alleles)
         elif self.ploidy == 3:
-            self.child_phased_GT, self.dad_phased_GT, self.mom_phased_GT = \
-                    phase_trio_trisomy(self.child_alleles, self.dad_alleles, self.mom_alleles) 
-            #TODO: make sure sibling_alleles == None in null case
-            if sibling_alleles != None:
-                self.sibling_phased_GT, _, _ = phase_trio_diploid(self.sibling_alleles, self.dad_alleles, self.mom_alleles)
-            pass
+            self.child_phased_GT, self.dad_phased_GT, self.mom_phased_GT, self.is_mendelian = \
+                    phase_trio_trisomy(self.child_alleles, self.dad_alleles, self.mom_alleles, self.nondisjunct_parent)
+
+            if not self.is_mendelian:
+                #if DS trio is not mendelian, set the sibling as unphased
+                if self.sibling_alleles is not None:
+                    self.sibling_phased_GT = '/'.join(self.sibling_alleles)
+            else:
+                if self.sibling_alleles is not None:
+                    self.sibling_phased_GT, self.dad_phased_GT, self.mom_phased_GT, _ = phase_trio_diploid(self.sibling_alleles, self.dad_alleles, self.mom_alleles)
+                if self.sibling_alleles is not None and '/' in self.child_phased_GT and '|' in self.sibling_phased_GT:
+                    # quartet sibling phased het position in nondisjunction parent
+                    self.is_phased_by_sibling = True
+                    if self.nondisjunct_parent == 'dad':
+                        self.child_phased_GT = self.dad_phased_GT + self.child_phased_GT[3:]
+                    else:
+                        self.child_phased_GT = self.child_phased_GT[:2] + self.mom_phased_GT
         else:
             raise Exception('Invalid ploidy in ' + self.__class__.__name__ + ' class: ' + str(self.ploidy))
+        
+        #set is_phased variable
+        if '/' not in self.child_phased_GT:
+            self.is_phased = True
+                
 
 
--- phase_main.py
+++ phase_main.py
@@ -24,16 +24,16 @@ if __name__ == '__main__':
     # read ped file
     rels = read_ped(ped_file)
 
-    # read vcf file and set nondisjunction parent
+    # read vcf file and find nondisjunction parent
     loci_by_chr, header = read_vcf(filename, rels, fam_size)
     trisomy_loci = [locus for loci_list in loci_by_chr.values() for locus in loci_list if locus.ploidy == 3]
-    nondisjunct_parent = find_nondisjunct_parent(trisomy_loci)
-    for locus in trisomy_loci:
-        locus.nondisjunct_parent = nondisjunct_parent
+    nondisjunct_parent = find_nondisjunct_parent(trisomy_loci) if len(trisomy_loci) > 0 else None
 
     # phase
     for loci_list in loci_by_chr.values():
         for locus in loci_list:
+            if locus.ploidy == 3:
+                locus.nondisjunct_parent = nondisjunct_parent
             locus.phase()
 
     # collect statistics and track phased regions
@@ -44,42 +44,28 @@ if __name__ == '__main__':
     dip_hom_mend_unphased = 0
     dip_nonmend = 0
     tri_het_mend_phased = 0
+    tri_het_mend_sib_phased = 0
     tri_het_mend_unphased = 0
     tri_het_mend_disj_phased = 0
     tri_hom_mend_phased = 0
     tri_hom_mend_unphased = 0
     tri_nonmend = 0
-    tri_phased_by_brother = 0
     for chrom, loci_list in loci_by_chr.items():
         curr_region = ['', '', '']
         for locus in loci_list: 
             if locus.is_mendelian:
                 alleles = set(locus.child_alleles + locus.dad_alleles + locus.mom_alleles)
-                if '/' in locus.child_phased_GT + locus.dad_phased_GT + locus.mom_phased_GT:
-                    if len(alleles) > 1:
-                        if locus.ploidy == 2:
-                            dip_het_mend_unphased += 1
-                        else:
-                            tri_het_mend_unphased += 1
-                            if '|' in locus.child_phased_GT:
-                                tri_het_mend_disj_phased += 1
-                    else:
-                        if locus.ploidy == 2:
-                            dip_hom_mend_unphased += 1
-                        else:
-                            tri_hom_mend_unphased += 1
-                    
-                    # reset current phased region
-                    if curr_region[1] != '':
-                        phased_regions_by_chr[locus.chrom].append(curr_region)
-                    curr_region = ['', '', '']
-
-                else:
-                    if len(alleles) > 1:
+                #if '/' in locus.child_phased_GT + locus.dad_phased_GT + locus.mom_phased_GT:
+                if locus.is_phased:
+                    # phased
+                    #if len(alleles) > 1:
+                    if locus.is_heterozygous:
                         if locus.ploidy == 2:
                             dip_het_mend_phased += 1
                         else:
                             tri_het_mend_phased += 1
+                            if locus.is_phased_by_sibling:
+                                tri_het_mend_sib_phased += 1
                     else:
                         if locus.ploidy == 2:
                             dip_hom_mend_phased += 1
@@ -92,6 +78,29 @@ if __name__ == '__main__':
                         curr_region[0] = locus.chrom
                         curr_region[1] = locus.pos
 
+                else:
+                    # unphased
+                    #if len(alleles) > 1:
+                    if locus.is_heterozygous:
+                        if locus.ploidy == 2:
+                            dip_het_mend_unphased += 1
+                        else:
+                            tri_het_mend_unphased += 1
+                            if '|' in locus.child_phased_GT:
+                                #should always occur (we phase 100% of the disjunction parent alleles), assertion at end
+                                tri_het_mend_disj_phased += 1
+                    else:
+                        #should never occur, assertion at end
+                        if locus.ploidy == 2:
+                            dip_hom_mend_unphased += 1
+                        else:
+                            tri_hom_mend_unphased += 1
+                    
+                    # reset current phased region
+                    if curr_region[1] != '':
+                        phased_regions_by_chr[locus.chrom].append(curr_region)
+                    curr_region = ['', '', '']
+
             else:
                 if locus.ploidy == 2:
                     dip_nonmend += 1
@@ -102,6 +111,8 @@ if __name__ == '__main__':
                 if curr_region[1] != '':
                     phased_regions_by_chr[locus.chrom].append(curr_region)
                 curr_region = ['', '', '']
+
+    #TODO: ADD TWO ASSERTIONS
 
     def print_division(print_str, numerator, denominator):
         print print_str,
